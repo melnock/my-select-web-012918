1f817dacd38c1930209027eca38444fc140261b2
Melissa Nock <melnock@gmail.com>
2018-01-17 21:44:34 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+ selection = []
+ i=0
+
+while i<collection.length
+  if (yield collection[i])
+    selection << collection[i]
+    i= i+1
+  else
+    i=i+1
+  end
+ end
+ 
+ selection
 end
